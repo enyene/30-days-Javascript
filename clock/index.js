b12638df59993ee78c90b3b2c12c7ebc933a3555
Enyene Akpan <akpanenyene@gmail.com>
2022-08-26 10:22:18 +0100

    work on app 'digital clock'

---
+++ clock/index.js
@@ -0,0 +1,33 @@
+let clock = () => {
+    let date = new Date();
+    let hrs = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    let time = `${hrs}:${min}:${sec}`;
+
+    hrs = hrs < 10 ? `0${hrs}` : hrs;
+    min = min < 10 ? `0${min}` : min;
+    sec = sec < 10 ? `0${sec}` : sec;
+    
+
+    let period = 'AM';
+if (hrs > 12) {
+    period = 'PM';
+}else{
+    period = 'AM';
+}
+
+
+
+    document.getElementById('clock').innerText = time;
+    setTimeout(clock, 1000);
+}
+
+clock();
+
+
+
+
+
+
+
